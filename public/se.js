ce87f50cb5ed5bdefa679df2861f7eea84658bd6
ramtin.zand <ramtinzand6@gmail.com>
2023-04-26 13:34:30 +0430

    cash

--- public/se.js
+++ public/se.js
@@ -1,12 +1,24 @@
-//sd
-self.addEventListener("install",e =>{
-    console.log("install")
-})
+const STATICCASHNAME = "site-static";
+const assets = ["/", "/index.html","/static/media/logo.6ce24c58023cc2f8fd88fe9d219db6c6.svg"];
+const self = this;
 
-self.addEventListener("activate",e=>{
-    console.log("active")
-})
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(STATICCASHNAME).then((cache) => {
+      console.log("cash");
+      cache.addAll(assets);
+    })
+  );
+});
 
-self.addEventListener("fetch",e=>{
-    console.log("fetch" , e)
-})
+self.addEventListener("activate", (e) => {
+  console.log("active");
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cachRes =>{
+        return cachRes || fetch(e.request)
+    })
+  )
+});
